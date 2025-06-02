ce75a0990647208eb6d5e299318c69bff041454e
execute <3xecvte@gmail.com>
2025-06-02 23:14:18 +0300

    feat: customer add customer state

--- Task2/src/main/java/esdc/sem4/multithreading/customer/Customer.java
+++ Task2/src/main/java/esdc/sem4/multithreading/customer/Customer.java
@@ -1,10 +1,12 @@
 package esdc.sem4.multithreading.customer;
 
+import esdc.sem4.multithreading.customer.state.CustomerState;
+
 import java.util.concurrent.Callable;
 
 public class Customer implements Callable<Void> {
     private final String name;
-//    private CustomerState state; TODO: add customer states
+    private CustomerState state;
     private final boolean isPreOrder;
 
     public Customer(String name, boolean isPreOrder) {
@@ -14,7 +16,12 @@ public class Customer implements Callable<Void> {
 
     @Override
     public Void call() throws Exception {
+        state.action();
         return null;
+    }
+
+    public void switchState(CustomerState state) {
+        this.state = state;
     }
 
     public String getName() {
