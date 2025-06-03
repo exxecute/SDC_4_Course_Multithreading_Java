4ac8b05cd4f97a887d8af3079f79896cfb3e0026
execute <3xecvte@gmail.com>
2025-06-03 19:37:21 +0300

    feat: cash register serving customer method

--- Task2/src/main/java/esdc/sem4/multithreading/restaurant/CashRegister.java
+++ Task2/src/main/java/esdc/sem4/multithreading/restaurant/CashRegister.java
@@ -1,22 +1,29 @@
 package esdc.sem4.multithreading.restaurant;
 
 import esdc.sem4.multithreading.customer.Customer;
+import esdc.sem4.multithreading.customer.state.CustomerServedState;
 
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 public class CashRegister implements Callable<Void> {
+    private static final int MAX_SERVING_TIME = 5; // TODO: get data from data loader
     private final int id;
     private final Queue<Customer> customerQueue = new PriorityQueue<Customer>();
+    private boolean isServing;
 
     public CashRegister(int id) {
         this.id = id;
+        this.isServing = false;
     }
 
     @Override
     public Void call() throws Exception {
-        // TODO: action for state
+        while(this.getIsServing()) {
+            this.serveTheCustomer();
+        }
         return null;
     }
 
@@ -32,5 +39,19 @@ public class CashRegister implements Callable<Void> {
         this.customerQueue.add(customer);
     }
 
+    public void setIsServing(boolean isServing) {
+        this.isServing = isServing;
+    }
 
+    public boolean getIsServing() {
+        return this.isServing;
+    }
+
+    private void serveTheCustomer() throws InterruptedException {
+        Customer currentCustomer = this.customerQueue.remove();
+        int servingTime = (int)(Math.random() * MAX_SERVING_TIME);
+        // TODO: add logger that this cash register start serving and it will take n seconds
+        TimeUnit.SECONDS.sleep(servingTime);
+        currentCustomer.switchState(new CustomerServedState(currentCustomer));
+    }
 }
