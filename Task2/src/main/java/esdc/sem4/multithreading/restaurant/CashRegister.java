fa5ac06f3c16acb2f4cf22e7b10920793a6ce7c0
execute <3xecvte@gmail.com>
2025-06-03 19:26:08 +0300

    feat: cash register add small logic, need to add states

--- Task2/src/main/java/esdc/sem4/multithreading/restaurant/CashRegister.java
+++ Task2/src/main/java/esdc/sem4/multithreading/restaurant/CashRegister.java
@@ -1,4 +1,36 @@
 package esdc.sem4.multithreading.restaurant;
 
-public class CashRegister {
+import esdc.sem4.multithreading.customer.Customer;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.Callable;
+
+public class CashRegister implements Callable<Void> {
+    private final int id;
+    private final Queue<Customer> customerQueue = new PriorityQueue<Customer>();
+
+    public CashRegister(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        // TODO: action for state
+        return null;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getQueueLength() {
+        return this.customerQueue.size();
+    }
+
+    public void addCustomer(Customer customer) {
+        this.customerQueue.add(customer);
+    }
+
+
 }
