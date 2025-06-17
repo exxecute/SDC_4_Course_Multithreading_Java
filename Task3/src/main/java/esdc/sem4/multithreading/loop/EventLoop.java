c849db133009188eae5476c6e479c386f17e04b1
execute <3xecvte@gmail.com>
2025-06-17 19:08:26 +0300

    task3: add event handler

--- Task3/src/main/java/esdc/sem4/multithreading/loop/EventLoop.java
+++ Task3/src/main/java/esdc/sem4/multithreading/loop/EventLoop.java
@@ -1,48 +1,38 @@
 package esdc.sem4.multithreading.loop;
 
-import esdc.sem4.multithreading.events.Event;
-import esdc.sem4.multithreading.events.LongRunningEvent;
+import esdc.sem4.multithreading.handler.EventHandler;
 import esdc.sem4.multithreading.queue.EventQueue;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class EventLoop implements Runnable {
     private final EventQueue eventQueue;
-    private final ExecutorService executorService;
+    private final EventHandler eventHandler;
     private boolean running = true;
 
     public EventLoop(EventQueue eventQueue, int threadPoolSize) {
         this.eventQueue = eventQueue;
-        this.executorService = Executors.newFixedThreadPool(threadPoolSize);
+        this.eventHandler = new EventHandler(threadPoolSize);
     }
 
     @Override
     public void run() {
         System.out.println("Event Loop started.");
-        while (running) {
+        while (this.running) {
             try {
-                Event event = eventQueue.takeEvent();
-                if(event instanceof LongRunningEvent) {
-                    System.out.println("Processing event in other thread: " + event.getClass().getSimpleName());
-                    executorService.submit(event::process);
-                } else {
-                    System.out.println("Processing event: " + event.getClass().getSimpleName());
-                    event.process();
-                }
-                TimeUnit.MILLISECONDS.sleep(10);
+                this.eventHandler.proccessEvent(this.eventQueue.takeEvent());
+                TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.err.println("Event Loop interrupted: " + e.getMessage());
-                running = false;
+                this.running = false;
             }
         }
-        executorService.shutdown();
+        this.eventHandler.shutdown();
         System.out.println("Event Loop stopped.");
     }
 
     public void stop() {
-        running = false;
+        this.running = false;
     }
 }
